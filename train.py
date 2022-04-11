78f013ce1db9cff28c3ebb05f0a91943c5200aa0
Nahuel <gugaco54@hotmail.com>
2022-04-11 12:26:18 +0200

    update in gitignore

--- train.py
+++ train.py
@@ -17,18 +17,18 @@ x_train = utils.normalise_data(x_train, np.min(x_train), np.max(x_train))
 y_train = np.load("./data/y_train_"+chemistry+".npy")
 
 # ------------- RANDOM FOREST -------------
-model = models.RandomForest.RandomForest(max_depth=10, random_state=42, n_estimators=100)
+model = RandomForest.RandomForest(max_depth=10, random_state=42, n_estimators=100)
 model.fit(x_train, y_train)
 model.save_model("./models/model-RF_"+chemistry)
 
 # ----------------- CNN_1D -----------------
-model = models.CNN_1D.CNN_1D(size)
+model = CNN_1D.CNN_1D(size)
 model.fit(x_train, y_train)
 model.load_weights('./checkpoints/checkpoint')
 model.save('./models/model-CNN_'+chemistry+'.h5')
 model.save_model("./models/model-CNN_1D_"+chemistry)
 
 # ------------------- FFN -------------------
-model = models.FFN.FFN(size)
+model = FFN.FFN(size)
 model.fit(x_train, y_train)
 model.save_model("./models/model-FFN_"+chemistry)
